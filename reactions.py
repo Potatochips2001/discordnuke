5559eec55ef095cb0a1383d091bfeac552086adc
Potatochips2001 <76017734+Potatochips2001@users.noreply.github.com>
2021-04-08 17:02:59 -0400

    reactions.py
    Auto react to every message

---
+++ reactions.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import time
+
+auth = {
+    "authorization": "" #your discord token
+}
+
+channelId = "" #replace with your channel id
+
+ids = []
+
+while True:
+    packet = requests.get(f"https://discord.com/api/v8/channels/{channelId}/messages?limit=1", headers=auth)
+    msgId = json.loads(packet.text)[0]['id']
+    if msgId not in ids:
+        packet = requests.put(f"https://discord.com/api/v8/channels/{channelId}/messages/{msgId}/reactions/🤪/@me", headers=auth)
+        print(packet)
+        ids.append(msgId)
+        time.sleep(0.25) #you can put whatever time you want here
